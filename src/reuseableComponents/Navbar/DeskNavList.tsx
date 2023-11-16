0eedb56322573e917666a39eac45fe15ccd46187
razarevel <razarevel@gmail.com>
2023-11-16 11:32:21 +0500

    Desk nav link update 2

--- src/reuseableComponents/Navbar/DeskNavList.tsx
+++ src/reuseableComponents/Navbar/DeskNavList.tsx
@@ -1,29 +1,26 @@
 import { Link } from "react-router-dom";
 
 export default function DeskNavList() {
+  const navLinks = [
+    { text: "Home", links: "/" },
+    { text: "Courses", links: "/courses" },
+    { text: "About Us", links: "/about" },
+    { text: "Pricing", links: "/pricing" },
+    { text: "Contact", links: "/contact" },
+  ];
   return (
     <>
       <div className="flex items-center justify-between space-x-8">
-        <Link to={"/"} className="group cursor-pointer space-y-1">
-          <p className="text-[1.2rem]">Home</p>
-          <div className="w-full h-0.5 bg-black rounded-full scale-x-0 duration-300 group-hover:scale-x-[80%]"></div>
-        </Link>
-        <Link to={"/courses"} className="group cursor-pointer space-y-1">
-          <p className="text-[1.2rem]">Courses</p>
-          <div className="w-full h-0.5 bg-black rounded-full scale-x-0 duration-300 group-hover:scale-x-[80%]"></div>
-        </Link>
-        <Link to={"/about"} className="group cursor-pointer space-y-1">
-          <p className="text-[1.2rem]">About Us</p>
-          <div className="w-full h-0.5 bg-black rounded-full scale-x-0 duration-300 group-hover:scale-x-[80%]"></div>
-        </Link>
-        <Link to={"/pricing"} className="group cursor-pointer space-y-1">
-          <p className="text-[1.2rem]">Pricing</p>
-          <div className="w-full h-0.5 bg-black rounded-full scale-x-0 duration-300 group-hover:scale-x-[80%]"></div>
-        </Link>
-        <Link to={"/contact"} className="group cursor-pointer space-y-1">
-          <p className="text-[1.2rem]">Contact</p>
-          <div className="w-full h-0.5 bg-black rounded-full scale-x-0 duration-300 group-hover:scale-x-[80%]"></div>
-        </Link>
+        {navLinks.map((el, index) => (
+          <Link
+            to={el.links}
+            className="group cursor-pointer space-y-1"
+            key={index}
+          >
+            <p className="text-[1.2rem]">{el.text}</p>
+            <div className="w-full h-0.5 bg-black rounded-full scale-x-0 duration-300 group-hover:scale-x-[80%]"></div>
+          </Link>
+        ))}
       </div>
     </>
   );
