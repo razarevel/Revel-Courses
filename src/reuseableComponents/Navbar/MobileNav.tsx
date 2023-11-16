444544abc60d69611c562ed4ffee0e6645a28575
razarevel <razarevel@gmail.com>
2023-11-16 11:35:45 +0500

    added hover on nav btn

--- src/reuseableComponents/Navbar/MobileNav.tsx
+++ src/reuseableComponents/Navbar/MobileNav.tsx
@@ -17,7 +17,7 @@ export default function MobileNav() {
           <Link to={"/login"}>Sign up</Link>
           <Link
             to={"/login"}
-            className="bg-[#FF9500] px-4 py-2 rounded-lg text-white duration-300 hover:bg-yellow-500"
+            className="bg-[#FF9500] px-4 py-2 rounded-lg text-white duration-300 hover:opacity-80"
           >
             Login
           </Link>
