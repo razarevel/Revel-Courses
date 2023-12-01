537c801cf5546249403b930f44ba034df7c5d75f
razarevel <razarevel@gmail.com>
2023-12-01 09:33:06 +0500

    adding images for courses open page

--- src/Pages/CoursesOpenPage/CoursesOpenPageMain.tsx
+++ src/Pages/CoursesOpenPage/CoursesOpenPageMain.tsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
 import useCounter from "../../Slug";
+import axios from "axios";
+import img from "./CoursesOpenPageComponents/igor-miske-Px3iBXV-4TU-unsplash.jpg";
 export default function CoursesOpenPageMain() {
   const { Slug } = useCounter();
-  return <>Welcome to courses open page of {Slug}</>;
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/api/courses/${Slug}`)
+      .then((res) => console.log(res.data.data.course));
+  }, []);
+  return (
+    <div className="">
+      <img src={""} alt="" className="w-[70%]" />
+    </div>
+  );
 }
